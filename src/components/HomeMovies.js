bdaca4f2b0afbc78bd8716eac3b897688c19463f
Mirjana Vukojevic <mirjana.vukojevic@quantox.com>
2019-05-24 16:21:30 +0200

    fixing movie page

--- src/components/HomeMovies.js
+++ src/components/HomeMovies.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import classes from '../assets/Global.module.css';
 
 class HomeMovies extends Component {
     state = { 
@@ -13,18 +14,40 @@ class HomeMovies extends Component {
         .then(response => response.json())
         .then(response => {
           this.setState({
-            movies: response,
-            avatarUrl: response.backdrop_path
+            movies: response.results,
           })
           console.log(this.state.movies)
+          console.log('avatar url',this.state.avatarUrl)
          })
     }
     render() { 
-        return ( 
-            <div>
-                <img src={require(`img/${this.state.avatarUrl}`)} width="60" />
-            </div>
-         );
+        if (this.state.movies.length > 0){
+            return ( 
+                <div>
+                    <div className="row">
+                        <div className={classes.firstMovie} style={{
+                            backgroundImage: `url("https://image.tmdb.org/t/p/original${this.state.movies[0].backdrop_path}")`,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'cover'
+                        }}>
+                            <div className={classes.firstText}>
+                                <h1>{this.state.movies[0].original_title}</h1>
+                                {this.state.movies[0].overview}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                     {this.state.movies.map((mira, key) => <div className="col-sm-3"><img key={key}  src={`https://image.tmdb.org/t/p/w500${mira.poster_path}`}/></div>)}
+                    </div>
+                    {/* {
+                    this.state.avatarUrl && <img src={`https://image.tmdb.org/t/p/w500/${this.state.avatarUrl[0].id}`} width="60" />} */}
+                    
+                </div>
+             );
+        } else {
+            return <div>LOADING!!!</div>
+        }
+        
     }
 }
  
